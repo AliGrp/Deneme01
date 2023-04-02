5bf6287f780c775f032869964c6b1c1e858145fd
techproed <burcupiaf@gmail.com>
2023-04-02 13:30:17 -0500

    burcu deneme

--- src/test/java/Deneme.java
+++ src/test/java/Deneme.java
@@ -1,4 +1,20 @@
 public class Deneme {
     // Hello world
     // Hello world....
+
+    public static void main(String[] args) {
+
+
+
+        int a=12;
+        int b=24;
+        System.out.println(a+b);
+
+
+
+    }
+
+
+
+
 }
